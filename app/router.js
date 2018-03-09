7fb06e8fcd345bc3079caaaf8bf50037a2511416
panxuelin <panxuelin2006@163.com>
2018-03-09 16:39:44 +0800

    服务器应用部署

--- app/router.js
+++ app/router.js
@@ -5,10 +5,10 @@
  */
 module.exports = app => {
     const {router, controller} = app;
-    router.get('/', controller.home.index);
-    router.get('/news', controller.news.list);
-    router.get('/user/index', controller.user.info);
-    router.get('/user/register', controller.user.register);
-    router.get('/user/register_async', controller.user.register_async);
+    router.get('/cat/', controller.home.index);
+    router.get('/cat/news', controller.news.list);
+    router.get('/cat/user/index', controller.user.info);
+    router.get('/cat/user/register', controller.user.register);
+    router.get('/cat/user/register_async', controller.user.register_async);
 
 };
